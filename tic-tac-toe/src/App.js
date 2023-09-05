772d487d083a9d600d511f32f76c3cb0b924b1a7
AvinashKalmegh <avinashkalmegh93@gmail.com>
2023-09-05 20:03:29 +0530

    created required components and did some styling to them

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -1,10 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import Board from './components/Board';
+import { useState } from 'react';
+
 
 function App() {
+
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [xPlaying, setXPlaying] = useState(true);
+
+  const Win_Conditions = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+  ]
+
+  const handleBoxClick = (boxIdx)=>{
+      const updateBoard = board.map((value, index)=>{
+          if(index == boxIdx){
+            return xPlaying === true ? "X" : "O";
+          }
+          else{
+            return value
+          }
+      })
+
+      setBoard(updateBoard);
+      setXPlaying(!xPlaying);
+  }
+
   return (
     <div className="App">
-      
+      <Board board={board} onClick={handleBoxClick} />
     </div>
   );
 }
